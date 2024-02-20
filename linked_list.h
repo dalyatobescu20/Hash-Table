9d56ab57c2f0ea6cff64918d668d73e100c0d37d
Tobescu Dalya <94745479+dalyatobescu20@users.noreply.github.com>
2024-02-20 17:10:35 +0200

    Update linked_list.h

--- linked_list.h
+++ linked_list.h
@@ -1,38 +1,36 @@
-/*Tobescu Dalya-Alexandra- grupa 311CB*/
 #pragma once
 
+// Structure for a node in the linked list
 typedef struct Node Node;
 struct Node {
-    void* data; //adresa informatie
-    Node* next;  //adresa urmatoarei celule
+    void* data; // Address of the data
+    Node* next; // Address of the next node
 };
 
+// Structure for the linked list
 typedef struct {
-    Node* head;  //pointer catre primul elem din lista
-    ssize_t data_size; //dimensiunea adresei de informatie
-    ssize_t size; //dimensiunea listei
+    Node* head; // Pointer to the first element in the list
+    ssize_t data_size; // Size of the data address
+    ssize_t size; // Size of the list
 } List;
 
+// Structure for a list of words with the same length
 typedef struct {
-    ssize_t w_len;//dimensiunea unui cuvant
-    List* list;//lista de cuvinte de aceeasi lungime
-}word_list;
+    ssize_t w_len; // Length of the word
+    List* list; // List of words with the same length
+} word_list;
 
+// Structure for a word
 typedef struct {
-    ssize_t count;//frecventa cuvantului
-    char* string;//cuvantul
+    ssize_t count; // Frequency of the word
+    char* string; // The word itself
 } Word;
 
+// Function prototypes
 List* CreateList(ssize_t data_size);
-
 void AddNode(List* list, ssize_t n, const void* new_data);
-
 Node* GetNode(List* list, ssize_t n);
-
 Node* RemoveNode(List* list, ssize_t n);
-
 void FreeList(List** pp_list);
-
 void SwapNodes(List* list, ssize_t n1, ssize_t n2);
-
 void SortList(List* list, int (*compare_func)(void*, void*));
